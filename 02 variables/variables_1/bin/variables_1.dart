3f901e1c8d483f06179342877569199e2d9cf920
FabianJoseBlanco <fabians_blanco@hotmail.com>
2024-12-16 12:17:24 -0500

    16/12/2024 02 variables_02

---
+++ 02 variables/variables_1/bin/variables_1.dart
@@ -0,0 +1,9 @@
+import 'package:variables_1/variables_1.dart' as variables_1;
+
+void main(List<String> arguments) {
+  var nombre = "luis";
+  var edad = 29;
+  print("mi nombre es: $nombre");
+  print(edad.runtimeType);
+  print(nombre.runtimeType);
+}
