a775ae79d248946eccf0ae8e13b7effe70ac592d
FabianJoseBlanco <fabians_blanco@hotmail.com>
2024-12-18 22:16:10 -0500

    18/12/2024 tipos de varibles

---
+++ 02 variables/variables_2/variables_2/bin/variables_2.dart
@@ -0,0 +1,20 @@
+import 'package:variables_2/variables_2.dart' as variables_2;
+
+void main(List<String> arguments) {
+  String nombre = "luis";
+  var name = "luis";
+  int edad = 29;
+  double estatura = 1.72;
+  bool esBuenProfesor = true;
+
+  double peso;
+  peso = 12.3;
+
+  final segundoApellido = "Baena";
+  const mesesDelAnio = 12;
+
+  print("Hola Mundo");
+  print(peso);
+  print(segundoApellido);
+  print(mesesDelAnio);
+}
