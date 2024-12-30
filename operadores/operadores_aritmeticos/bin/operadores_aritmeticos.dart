87c0a6f583b79794a44ad2f1b7235cb81661359a
FabianJoseBlanco <fabians_blanco@hotmail.com>
2024-12-30 06:01:36 -0500

    21/12/2024 Operadores operadores aritmeticos

---
+++ operadores/operadores_aritmeticos/bin/operadores_aritmeticos.dart
@@ -0,0 +1,17 @@
+import 'package:operadores_aritmeticos/operadores_aritmeticos.dart'
+    as operadores_aritmeticos;
+
+void main(List<String> arguments) {
+  int a = 10;
+  int b = 25;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(b / a);
+
+  //Incremento y decremento
+  a++;
+  print(a);
+  a--;
+  print(a);
+}
